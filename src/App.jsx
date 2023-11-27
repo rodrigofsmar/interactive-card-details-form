2e81ea3b4a1862459668925f9806fb619e77d4a5
rodrigofsmar <rodrigosalazarmarcelino@gmail.com>
2023-11-27 10:58:34 +0000

    updated default variables

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ import CreditCard from './components/CreditCard/CreditCard'
 
 function App() {
 
-  const [cardNumber, setCardNumber] = useState("0000 0000 0000 0000");
-  const [name, setName] = useState("Mary Smith");
-  const [expirationDate, setExpirationDate] = useState("09 / 23");
+  const defaultCardNumber = "0000 0000 0000 0000";
+  const defaultName = "Mary Smith";
+  const defaultExpirationDate = "09 / 23";
+
+  const [cardNumber, setCardNumber] = useState(defaultCardNumber);
+  const [name, setName] = useState(defaultName);
+  const [expirationDate, setExpirationDate] = useState(defaultExpirationDate);
 
   const handleCardNumberUpdate = (event) =>{
     const maxLength = 16;
-
     const newCardNumber = event.target.value
                           .replace(/[^0-9]/g, '')
                           .slice(0, maxLength);
 
     if (newCardNumber == ""){
-      setCardNumber("0000 0000 0000 0000") //WHEN THE INPUT IS EMPTY, SET IT TO "0000 0000 0000 0000"
+      setCardNumber(defaultCardNumber) //WHEN THE INPUT IS EMPTY, SET IT TO "0000 0000 0000 0000"
     }else{
       const formattedCardNumber = formatCardNumber(newCardNumber);
       setCardNumber(formattedCardNumber);
@@ -35,7 +38,7 @@ function App() {
   const handleNameUpdate = (event) =>{
     const newName = event.target.value;
     if (newName == ""){
-      setName("Mary Smith")
+      setName(defaultName)
     }else{
       setName(newName);
     }
@@ -44,7 +47,7 @@ function App() {
   const handleExpirationDateUpdate = (event) =>{
     const newExpirationDate = event.target.value;
     if (newExpirationDate == ""){
-      setExpirationDate("09 / 23")
+      setExpirationDate(defaultExpirationDate)
     }else{
       setExpirationDate(newExpirationDate);
     }
@@ -58,10 +61,10 @@ function App() {
           <CreditCard cardNumber={cardNumber} name={name} expirationDate={expirationDate}></CreditCard>
         </div>
         <div className='form-col col-xs-12 col-sm-12 col-md-8'>
-          <form class="needs-validation" novalidate>
+          <form className="needs-validation" noValidate>
             <div className="form-group mb-3">
               <label htmlFor="cardNumber">CARD NUMBER</label>
-              <input onChange={handleCardNumberUpdate} value={cardNumber === "0000 0000 0000 0000" ? "" : cardNumber}  className="form-control" id="cardNumber" placeholder="e.g. 1234 5678 9123 0000"/>
+              <input onChange={handleCardNumberUpdate} value={cardNumber === defaultCardNumber ? "" : cardNumber}  className="form-control" id="cardNumber" placeholder="e.g. 1234 5678 9123 0000"/>
             </div>
             <div className="form-group mb-3">
                 <label htmlFor="cardholderName">CARDHOLDER NAME</label>
@@ -70,14 +73,14 @@ function App() {
             <div className="row mb-3">
               <div className="col-6">
                 <label htmlFor='expirationDate' className="form-label">EXPIRATION DATE (MM/YY)</label>
-                <input onChange={handleExpirationDateUpdate} type='text' className='form-control' id='expirationDate' placeholder='MM / YY' pattern="\d{2} / \d{2}" required></input>
+                <input onChange={handleExpirationDateUpdate} value={expirationDate === defaultExpirationDate ? "" : expirationDate} type='text' className='form-control' id='expirationDate' placeholder='MM / YY' pattern="\d{2} / \d{2}" required></input>
               </div>
               <div className='col-6'>
                 <label className="form-label">CVC</label>
                 <input type="text" className="form-control" id="expirationYear" placeholder="e.g. 123" pattern="\d{3}" required />
               </div>
             </div>
-            <button type="submit" class="btn btn-primary w-100">Submit</button>
+            <button type="submit" className="btn btn-primary w-100">Submit</button>
           </form>
         </div>
       </div>
